8a063ca2485522aa66a573e94e4a221cadacdf43
gshorten <geoff.shorten@shortens.ca>
2021-05-12 13:17:04 -0600

    tweak wifi keep alive

--- ESP32_Sonos_Controller/WiFi.ino
+++ ESP32_Sonos_Controller/WiFi.ino
@@ -4,12 +4,14 @@ boolean initWiFi() {
   //connects to the wifi network
   boolean wifiConnected = false;
   // attempt to connect using saved values for ssid and pwd
-  WiFi.begin(conf.values[0].c_str(), conf.values[1].c_str());
+  // WiFi.begin(conf.values[0].c_str(), conf.values[1].c_str());
   int wifiAttempts = 0;
   int maxAttempts = 7;
   while (WiFi.status() != WL_CONNECTED && wifiAttempts < maxAttempts) {   //loop until wifi is connected OR max attempts
     wifiAttempts ++;
     delay(750);
+    // attempt to connect using saved values for ssid and pwd
+    WiFi.begin(conf.values[0].c_str(), conf.values[1].c_str());
     Serial.print(".");
   }
   // if we are connected return true
@@ -27,19 +29,19 @@ boolean initWiFi() {
   return wifiConnected;
 }
 
-void wifiKeepAlive(){
+void wifiKeepAlive() {
   //checks to see if wifi is connected, if not it reconnects
   const long checkFreq = 5000;
   static long lastCheck = millis();
-  if (millis() - lastCheck > checkFreq){
-    if (WiFi.status() != WL_CONNECTED){
+  if (millis() - lastCheck > checkFreq) {
+    if (WiFi.status() != WL_CONNECTED) {
       // Reconnect
       int wifiAttempts = 0;
       int maxAttempts = 5;
       while (WiFi.status() != WL_CONNECTED && wifiAttempts < maxAttempts) {   //loop until wifi is connected OR max attempts
+        WiFi.begin(conf.values[0].c_str(), conf.values[1].c_str());
         wifiAttempts ++;
         delay(500);
-        WiFi.begin(conf.values[0].c_str(), conf.values[1].c_str());
         Serial.print(".");
       }
     }
